4b36a9b19e0a49a0b1d15a439e4981e43f041b64
Adrian Gao <mingze.gao@sydney.edu.au>
2023-12-06 10:31:22 +1100

    Update cmd_compute.py

--- src/mktstructure/cmd_compute.py
+++ src/mktstructure/cmd_compute.py
@@ -1,5 +1,5 @@
 import argparse
-import os
+from typing import Callable
 from datetime import datetime as dt
 from functools import partial
 import pandas as pd
@@ -19,106 +19,91 @@ def afunc(func, params):
     return func(df)
 
 
-def compute(measure, data):
+def _compute(measure, data, *func_args, out_filepath="results.csv", **func_kwargs):
+    assert hasattr(measure, "estimate")
+    assert isinstance(measure.estimate, Callable)
     results = parx(partial(afunc, measure.estimate), data)
-    for res, (ric, date, _) in zip(results, data):
-        print(ric, date, measure.name, res)
+
+    with open(out_filepath, "w", encoding="utf-8") as fout:
+        for res, (ric, date, _) in zip(results, data):
+            # Variance ratio test returns a list of results
+            if measure.name == "LoMacKinlay1988":
+                assert isinstance(res, list)
+                for r in res:
+                    for k, v in r.items():
+                        formated = format_result(date, ric, k, v)
+                        print(formated, file=fout)
+            else:
+                result_formated = format_result(date, ric, measure.name, res)
+                print(result_formated, file=fout)
+
+
+def get_trades_and_quotes(args: argparse.Namespace):
+    """
+    Retrieves and processes trade and quote files based on specified arguments.
+
+    This function processes files located in a specified data directory. It 
+    filters and returns signed trades and quotes based on a given set of 
+    criteria. The criteria include the type of files to process, a date range, 
+    and specific RICs (Reuters Instrument Codes).
+
+    Parameters:
+    args (argparse.Namespace): A namespace object from argparse containing 
+        the following attributes:
+        - data_dir (str): Directory where the data files are located.
+        - ric (list of str): List of RICs to filter the data.
+        - b (str): Beginning date in ISO format (inclusive) for filtering data.
+        - e (str): Ending date in ISO format (inclusive) for filtering data.
+        - all (bool): Flag to indicate whether to process all data without 
+            date and RIC filtering.
+
+    Returns:
+    tuple: A tuple containing two lists:
+        - signed_trades (list): A list of tuples (ric, date, file_path) for 
+            signed trades that meet the filtering criteria.
+        - quotes (list): A list of tuples (ric, date, file_path) for quotes 
+            that meet the filtering criteria.
+    """
+    signed_trades = process_files(
+        args.data_dir, file_pattern="*.signed-trades.csv.gz")
+    quotes = process_files(
+        args.data_dir, file_pattern="*.quotes.csv.gz")
+
+    if not args.all:
+        bdate = dt.fromisoformat(args.b)
+        edate = dt.fromisoformat(args.e)
+        signed_trades = [
+            (ric, date, file_path) for ric, date, file_path in signed_trades
+            if ric in args.ric and bdate <= dt.fromisoformat(date) <= edate]
+        quotes = [
+            (ric, date, file_path) for ric, date, file_path in quotes
+            if ric in args.ric and bdate <= dt.fromisoformat(date) <= edate]
+
+    return signed_trades, quotes
 
 
 def cmd_compute(args: argparse.Namespace):
 
-    # Filter files to work on
-    files = process_files(args.data_dir, file_pattern="*.signed.csv.gz")
-    if not args.all:
-        bdate = dt.fromisoformat(args.b)
-        edate = dt.fromisoformat(args.e)
-        files = [
-            (ric, date, file_path) for ric, date, file_path in files
-            if ric in args.ric and bdate <= dt.fromisoformat(date) <= edate]
+    signed_trades, quotes = get_trades_and_quotes(args)
+    compute = partial(_compute, out_filepath=args.out)
 
-    if args.bid_ask_spread:
-        compute(measures.bidask_spread, files)
-
-    # Execute
-    with open(args.out, "w", encoding="utf-8") as fout:
-        pass
-
-
-def cmd_compute_old(args: argparse.Namespace):
-
-    if args.out:
-        fout = open(args.out, "w")
-
-    for root, _, files in os.walk(args.data_dir):
-        for f in files:
-            # skip those unsigned ones
-            # TODO: .csv vs .csv.gz
-            if ".csv" not in f:
-                continue
-            if not (
-                args.bid_slope
-                or args.ask_slope
-                or args.scaled_depth_diff_1
-                or args.scaled_depth_diff_5
-            ):
-                if "signed" not in f:
-                    continue
-
-            path = os.path.join(root, f)
-            ric, date = os.path.normpath(path).split(os.sep)[-2:]
-            date = dt.fromisoformat(
-                date.removesuffix(".csv")
-                .removesuffix(".csv.gz")
-                .removesuffix(".signed")
-                .removesuffix(".sorted")
-            )
-
-            # if `--all` flag is set
-            if not args.all:
-                if ric not in args.ric:
-                    continue
-
-                if not (dt.fromisoformat(args.b) <= date <= dt.fromisoformat(args.e)):
-                    continue
-
-            if os.path.isfile(path):
-                df = pd.read_csv(path)
-
-                if args.bid_ask_spread:
-                    _compute(measures.bidask_spread, path, date, ric, df, fout)
-                if args.effective_spread:
-                    _compute(measures.effective_spread,
-                             path, date, ric, df, fout)
-                if args.realized_spread:
-                    _compute(measures.realized_spread,
-                             path, date, ric, df, fout)
-                if args.price_impact:
-                    _compute(measures.price_impact, path, date, ric, df, fout)
-                if args.variance_ratio:
-                    _compute(measures.variance_ratio,
-                             path, date, ric, df, fout)
-                if args.bid_slope:
-                    _compute(measures.bid_slope, path, date, ric, df, fout)
-                if args.ask_slope:
-                    _compute(measures.ask_slope, path, date, ric, df, fout)
-                if args.scaled_depth_diff_1:
-                    _compute(measures.sdd1, path, date, ric, df, fout)
-                if args.scaled_depth_diff_5:
-                    _compute(measures.sdd5, path, date, ric, df, fout)
-
-    fout.close()
-
-
-def _compute(measure, path, date, ric, data, fout):
-    print(f"Computing {measure.name} for {path}")
-    result = measure.estimate(data)
-    # Variance ratio test returns a list of results
-    if measure.name == "LoMacKinlay1988":
-        assert isinstance(result, list)
-        for res in result:
-            for k, v in res.items():
-                formated = format_result(date, ric, k, v)
-                print(formated, file=fout)
-    else:
-        result_formated = format_result(date, ric, measure.name, result)
-        print(result_formated, file=fout)
+    # quoted-spread based on quotes not trades
+    if args.quoted_spread:
+        compute(measures.quoted_spread, quotes)
+    # others use signed trades (and quotes)
+    if args.effective_spread:
+        compute(measures.effective_spread, signed_trades)
+    if args.realized_spread:
+        compute(measures.realized_spread, signed_trades)
+    if args.price_impact:
+        compute(measures.price_impact, signed_trades)
+    if args.variance_ratio:
+        compute(measures.variance_ratio, signed_trades)
+    if args.bid_slope:
+        compute(measures.bid_slope, signed_trades)
+    if args.ask_slope:
+        compute(measures.ask_slope, signed_trades)
+    if args.scaled_depth_diff_1:
+        compute(measures.sdd1, signed_trades)
+    if args.scaled_depth_diff_5:
+        compute(measures.sdd5, signed_trades)
