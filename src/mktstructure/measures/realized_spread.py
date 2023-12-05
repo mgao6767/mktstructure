7c3b8246e9a0e09db26b19daff34d5e1ea157f09
Adrian Gao <mingze.gao@sydney.edu.au>
2023-12-06 10:30:36 +1100

    Spreads using frds

--- src/mktstructure/measures/realized_spread.py
+++ src/mktstructure/measures/realized_spread.py
@@ -1,17 +1,10 @@
 import numpy as np
 import pandas as pd
 
+from frds.measures.spread import realized_spread
 from .exceptions import *
 
 name = "RealizedSpread"
-description = """
-The realized spread is the temporary component of the effective spread. 
-It measures the revenue to liquidity providers assuming that the liquidity provider is able to close
-her position at the midpoint prevailing five minutes after the trade:
-2 * q * (P - midpoint_5min)
-where q is the trade direction (1 for buys and -1 for sells),
-P is the transaction price, and midpoint_5min is the bid-ask midpoint 5mins later. 
-"""
 vars_needed = {"Price", "Volume", "Mid Point", "Direction"}
 
 
@@ -26,13 +19,11 @@ def estimate(data: pd.DataFrame) -> np.ndarray:
             if ts2 - ts1 >= np.timedelta64(5, "m"):
                 matched_midpt.append(midpt[idx + i])
                 break
+
     matched = len(matched_midpt)
-    rspread = (
-        2 * data["Direction"].to_numpy()[:matched] * (price[:matched] - matched_midpt)
-    )
-    # Daily realized spread is the dollar-volume-weighted average
-    # of the realized spread computed over all trades in the day.
-    volume = data["Volume"].to_numpy()
-    dolloar_volume = np.multiply(volume, price)[:matched]
-    rsprd = np.sum(np.multiply(rspread, dolloar_volume) / np.sum(dolloar_volume))
-    return np.nan if np.isnan(rsprd) else rsprd
+    direction = data["Direction"].to_numpy()[:matched]
+    volume = data["Volume"].to_numpy()[:matched]
+    midpt = midpt[:matched]
+    price = price[:matched]
+
+    return realized_spread(price, matched_midpt, midpt, volume, direction, pct_spread=True)
